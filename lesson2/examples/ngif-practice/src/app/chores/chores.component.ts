6d7eb7526fc6131db28f281980fdfe7e9ab6013a
Daniel C <daniel@daniel.com>
2019-10-25 12:47:37 -0500

    Lesson 2 ngIf practice complete.

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   chores = ['Empty dishwasher', 'Complete LaunchCode prep work','feed fish'];
+   //chores = [];
+   //finishedChores = ['Make Beds', 'Take Out Trash', 'Clean Litter Box', 'Clean Bathroom', 'Rake leaves', 'Bale Hay'];
+   //finishedChores = ['Make Beds', 'Take Out Trash', 'Buy groceries'];
+   finishedChores = ['Make Beds', 'Buy groceries'];
 
    trophyImage = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
 
@@ -17,3 +20,4 @@ export class ChoresComponent implements OnInit {
    }
 
 }
+
