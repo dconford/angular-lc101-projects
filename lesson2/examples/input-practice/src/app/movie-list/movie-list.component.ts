4f487e7ad88844a6b4896f8eca734bb6dd562247
Daniel C <daniel@daniel.com>
2019-10-25 20:29:25 -0500

    finished lesson 2 examples

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,32 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   // addMovie (newTitle: string) {
+   //    if(!this.movies.includes(newTitle) && (newTitle.length!==0)){
+   //       this.movies.push(newTitle);
+   //    }
+   // }
+
+
+ // The following code is one solution to the bonus task.
+   // Comment out lines 16 - 20, then uncomment lines 25 - 35 to use it.
+   
+   addMovie (newTitle: string) {
+      let errorMsg = '';
+      if(newTitle === ''){
+         errorMsg = 'Please enter a movie title.';
+      } else if (this.movies.includes(newTitle)) {
+         errorMsg = `${newTitle} is already in your movie list.`;
+      } else {
+         this.movies.push(newTitle);
+      }
+      return errorMsg;
+   }
+   
 }
+// Modify addMovie to reject the empty string as a title.
+// Use *ngIf to display an error message if the user does not enter a title or submits a title that is already on the list.
+// Add CSS to change the color of the error message.
+
+// The example-solutions branch of the Angular repo shows completed code for the bonus tasks.
